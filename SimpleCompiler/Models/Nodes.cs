b64319c9a95fe4e902226d356b334a37da9a79e3
Lamadelrae <matthew.almeida572@gmail.com>
2023-09-08 14:41:43 -0300

    feature: converting AST to typed AST for easier code generation.

--- SimpleCompiler/Models/Nodes.cs
+++ SimpleCompiler/Models/Nodes.cs
@@ -1,25 +1,77 @@
-﻿using SimpleCompiler.Enums;
-
+﻿
 namespace SimpleCompiler.Models.Nodes;
-
-public struct Loc
-{
-    public int Start { get; set; }
-    public int End { get; set; }
-    public string Filename { get; set; }
-}
-
-public abstract class Term
-{
-    public Kind Kind { get; protected set; }
-    public Loc Location { get; set; }
-}
 
 public class File
 {
     public string Name { get; set; }
     public Term Expression { get; set; }
     public Loc Location { get; set; }
+}
+
+public class Loc
+{
+    public int Start { get; set; }
+    public int End { get; set; }
+    public string FileName { get; set; }
+
+    public static Loc From(dynamic node)
+    {
+        return new Loc
+        {
+            Start = node.start,
+            End = node.end,
+            FileName = node.filename,
+        };
+    }
+}
+
+
+public enum BinaryOp
+{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    Eq,
+    Neq,
+    Lt,
+    Gt,
+    Lte,
+    Gte,
+    And,
+    Or
+}
+
+public abstract class Term
+{
+    public string Kind { get; set; }
+    public Loc Location { get; set; }
+}
+
+public class Let : Term
+{
+    public LetName Name { get; set; }
+    public Term Value { get; set; }
+    public Term Next { get; set; }
+
+    public class LetName
+    {
+        public string Text { get; set; }
+        public Loc Location { get; set; }
+    }
+}
+
+public class Function : Term
+{
+    public List<Parameter> Parameters { get; set; }
+    public Term Value { get; set; }
+
+    public class Parameter
+    {
+        public string Text { get; set; }
+        public Loc Location { get; set; }
+    }
 }
 
 public class If : Term
@@ -29,11 +81,32 @@ public class If : Term
     public Term Otherwise { get; set; }
 }
 
-public class Let : Term
+public class Binary : Term
 {
-    public Parameter Name { get; set; }
+    public Term Lhs { get; set; }
+    public BinaryOp Op { get; set; }
+    public Term Rhs { get; set; }
+}
+
+public class Var : Term
+{
+    public string Text { get; set; }
+}
+
+public class Print : Term
+{
     public Term Value { get; set; }
-    public Term Next { get; set; }
+}
+
+public class Call : Term
+{
+    public Term Callee { get; set; }
+    public List<Term> Arguments { get; set; }
+}
+
+public class Int : Term
+{
+    public int Value { get; set; }
 }
 
 public class Str : Term
@@ -44,60 +117,4 @@ public class Str : Term
 public class Bool : Term
 {
     public bool Value { get; set; }
-}
-
-public class Int : Term
-{
-    public int Value { get; set; }
-}
-
-public class BinaryOp : Term
-{
-    public BinaryOpType Op { get; set; }
-    public Term Left { get; set; }
-    public Term Right { get; set; }
-}
-
-public class Call : Term
-{
-    public Term Callee { get; set; }
-    public List<Term> Arguments { get; set; }
-}
-
-public class Function : Term
-{
-    public List<Parameter> Parameters { get; set; }
-    public Term Value { get; set; }
-}
-
-public class Print : Term
-{
-    public Term Value { get; set; }
-}
-
-public class First : Term
-{
-    public Term Value { get; set; }
-}
-
-public class Second : Term
-{
-    public Term Value { get; set; }
-}
-
-public class Tuple : Term
-{
-    public Term First { get; set; }
-    public Term Second { get; set; }
-}
-
-public class Parameter
-{
-    public string Text { get; set; }
-    public Loc Location { get; set; }
-}
-
-public class Var : Term
-{
-    public string Text { get; set; }
 }
---
+++ SimpleCompiler/Services/TypedAstGenerator.cs
@@ -0,0 +1,136 @@
+﻿using SimpleCompiler.Models.Nodes;
+
+namespace SimpleCompiler.Services
+{
+    public static class TypedAstGenerator
+    {
+        public static Term Generate(dynamic dynamicNode)
+        {
+            string kind = dynamicNode.kind;
+            switch ((string)kind)
+            {
+                case "Let":
+                    return new Let
+                    {
+                        Kind = kind,
+                        Name = new Let.LetName
+                        {
+                            Text = dynamicNode.name.text,
+                            Location = Loc.From(dynamicNode.name.location)
+                        },
+                        Value = Generate(dynamicNode.value),
+                        Next = Generate(dynamicNode.next),
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Function":
+                    var functionNode = new Function
+                    {
+                        Kind = kind,
+                        Parameters = new List<Function.Parameter>(),
+                        Value = Generate(dynamicNode.value),
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                    foreach (var parameter in dynamicNode.parameters)
+                    {
+                        functionNode.Parameters.Add(new Function.Parameter
+                        {
+                            Text = parameter.text,
+                            Location = Loc.From(parameter.location)
+                        });
+                    }
+                    return functionNode;
+                case "If":
+                    return new If
+                    {
+                        Kind = kind,
+                        Condition = Generate(dynamicNode.condition),
+                        Then = Generate(dynamicNode.then),
+                        Otherwise = Generate(dynamicNode.otherwise),
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Binary":
+                    return new Binary
+                    {
+                        Kind = kind,
+                        Lhs = Generate(dynamicNode.lhs),
+                        Op = ConvertToBinaryOp(dynamicNode.op),
+                        Rhs = Generate(dynamicNode.rhs),
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Var":
+                    return new Var
+                    {
+                        Kind = kind,
+                        Text = dynamicNode.text,
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Print":
+                    return new Print
+                    {
+                        Kind = kind,
+                        Value = Generate(dynamicNode.value),
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Call":
+                    var callNode = new Call
+                    {
+                        Kind = kind,
+                        Callee = Generate(dynamicNode.callee),
+                        Arguments = new List<Term>(),
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                    foreach (var argument in dynamicNode.arguments)
+                    {
+                        callNode.Arguments.Add(Generate(argument));
+                    }
+                    return callNode;
+                case "Int":
+                    return new Int
+                    {
+                        Kind = kind,
+                        Value = dynamicNode.value,
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Str":
+                    return new Str
+                    {
+                        Kind = kind,
+                        Value = dynamicNode.value,
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                case "Bool":
+                    return new Bool
+                    {
+                        Kind = kind,
+                        Value = dynamicNode.value,
+                        Location = Loc.From(dynamicNode.location)
+                    };
+                // Add cases for other node kinds as needed
+                default:
+                    throw new NotSupportedException($"Unsupported node kind: {kind}");
+            }
+        }
+
+        private static BinaryOp ConvertToBinaryOp(dynamic dynamicOp)
+        {
+            string op = dynamicOp;
+            return op switch
+            {
+                "Add" => BinaryOp.Add,
+                "Sub" => BinaryOp.Sub,
+                "Mul" => BinaryOp.Mul,
+                "Div" => BinaryOp.Div,
+                "Rem" => BinaryOp.Rem,
+                "Eq" => BinaryOp.Eq,
+                "Neq" => BinaryOp.Neq,
+                "Lt" => BinaryOp.Lt,
+                "Gt" => BinaryOp.Gt,
+                "Lte" => BinaryOp.Lte,
+                "Gte" => BinaryOp.Gte,
+                "And" => BinaryOp.And,
+                "Or" => BinaryOp.Or,
+                _ => throw new NotSupportedException($"Unsupported binary operation: {op}")
+            };
+        }
+    }
+}
